c9c08c9f0067a7dcc216874072f25f16e27dbf0a
Michał Tuszyński <srgtuszy@gmail.com>
2022-02-10 00:00:48 +0100

    Added swift package file

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.5.0
+import PackageDescription
+
+let package = Package(
+    name: "Aerogear-iOS-HTTP",
+    platforms: [
+        .iOS(.v9), .macOS(.v12), 
+    ],
+    products: [
+        .library(
+            name: "AeroGearHttp",
+            targets: ["AeroGearHttp"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "AeroGearHttp",
+            dependencies: [],
+            path: "AeroGearHttp"
+        ),
+        .testTarget(
+            name: "AeroGearHttpTests",
+            dependencies: ["AeroGearHttp"],
+            path: "AeroGearHttpTests"
+        )
+    ]
+)
